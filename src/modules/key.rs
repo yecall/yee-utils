8101c5fe9126fd08a8bbcbe77777118232279113
guoxbin <guoxbin@gmail.com>
2020-07-16 16:17:13 +0800

    dest mini secret key, secret key, public key, address

--- src/modules/key.rs
+++ src/modules/key.rs
@@ -2,12 +2,14 @@ use clap::{Arg, ArgMatches, SubCommand};
 use rand::Rng;
 use rand::thread_rng;
 use serde::Serialize;
-use yee_primitives::{AddressCodec, Hrp};
+use yee_primitives::{AddressCodec, Hrp, Address};
 use yee_sharding_primitives::utils;
 use yee_signer::KeyPair;
 
 use crate::modules::{base, Command, Module};
 use crate::modules::base::Hex;
+
+const SHARD_COUNT_LIST: [u16; 2] = [4, 8];
 
 pub fn module<'a, 'b>() -> Module<'a, 'b> {
 	Module {
@@ -53,13 +55,37 @@ fn sub_commands<'a, 'b>() -> Vec<Command<'a, 'b>> {
 						.takes_value(true)
 						.required(true)),
 			f: generate,
+		},
+		Command {
+			app: SubCommand::with_name("mini_secret_key")
+				.about("Desc mini secret key")
+				.arg(Arg::with_name("INPUT").required(false).index(1)),
+			f: mini_secret_key,
+		},
+		Command {
+			app: SubCommand::with_name("secret_key")
+				.about("Desc secret key")
+				.arg(Arg::with_name("INPUT").required(false).index(1)),
+			f: secret_key,
+		},
+		Command {
+			app: SubCommand::with_name("public_key")
+				.about("Desc public key")
+				.arg(Arg::with_name("INPUT").required(false).index(1)),
+			f: public_key,
+		},
+		Command {
+			app: SubCommand::with_name("address")
+				.about("Desc address")
+				.arg(Arg::with_name("INPUT").required(false).index(1)),
+			f: address,
 		}
 	]
 }
 
 fn generate(matches: &ArgMatches) -> Result<Vec<String>, String> {
-	let shard_num = matches.value_of("SHARD_NUM").expect("qed").parse::<u16>().map_err(|_|"invalid shard num")?;
-	let shard_count = matches.value_of("SHARD_COUNT").expect("qed").parse::<u16>().map_err(|_|"invalid shard count")?;
+	let shard_num = matches.value_of("SHARD_NUM").expect("qed").parse::<u16>().map_err(|_| "invalid shard num")?;
+	let shard_count = matches.value_of("SHARD_COUNT").expect("qed").parse::<u16>().map_err(|_| "invalid shard count")?;
 
 	let (mini_secret_key, public_key, secret_key, address, testnet_address) = loop {
 		let mini_secret_key = random_32_bytes(&mut thread_rng());
@@ -68,11 +94,10 @@ fn generate(matches: &ArgMatches) -> Result<Vec<String>, String> {
 		let secret_key = key_pair.secret_key();
 		let address_shard_num = utils::shard_num_for_bytes(&public_key, shard_count);
 		if address_shard_num == Some(shard_num) {
-
 			let address = public_key.to_address(Hrp::MAINNET).map_err(|_e| "address encode failed")?;
 			let testnet_address = public_key.to_address(Hrp::TESTNET).map_err(|_e| "address encode failed")?;
 
-			break (mini_secret_key, public_key, secret_key, address, testnet_address)
+			break (mini_secret_key, public_key, secret_key, address, testnet_address);
 		}
 	};
 
@@ -87,7 +112,7 @@ fn generate(matches: &ArgMatches) -> Result<Vec<String>, String> {
 		testnet_address: String,
 	}
 
-	let output = Output{
+	let output = Output {
 		shard_num,
 		shard_count,
 		mini_secret_key: mini_secret_key.to_vec().into(),
@@ -95,6 +120,183 @@ fn generate(matches: &ArgMatches) -> Result<Vec<String>, String> {
 		public_key: public_key.to_vec().into(),
 		address: address.0,
 		testnet_address: testnet_address.0,
+	};
+
+	base::output(&output)
+}
+
+fn mini_secret_key(matches: &ArgMatches) -> Result<Vec<String>, String> {
+	let input = base::input_string(matches)?;
+
+	let input: Vec<u8> = input.parse::<Hex>().map_err(|_| "Convert failed")?.into();
+
+	let key_pair = KeyPair::from_mini_secret_key(&input.clone())?;
+
+	let secret_key = key_pair.secret_key();
+
+	let public_key = key_pair.public_key();
+
+	let address = public_key.to_address(Hrp::MAINNET).map_err(|_e| "address encode failed")?;
+	let testnet_address = public_key.to_address(Hrp::TESTNET).map_err(|_e| "address encode failed")?;
+
+	#[derive(Serialize)]
+	struct Shard {
+		shard_num: u16,
+		shard_count: u16,
+	}
+
+	let shard = SHARD_COUNT_LIST.iter().map(|&shard_count| {
+		let shard_num = utils::shard_num_for_bytes(&public_key, shard_count).expect("qed");
+		Shard {
+			shard_num,
+			shard_count,
+		}
+	}).collect::<Vec<_>>();
+
+	#[derive(Serialize)]
+	struct Output {
+		mini_secret_key: Hex,
+		secret_key: Hex,
+		public_key: Hex,
+		address: String,
+		testnet_address: String,
+		shard: Vec<Shard>,
+	}
+
+	let output = Output {
+		mini_secret_key: input.into(),
+		secret_key: secret_key.to_vec().into(),
+		public_key: public_key.to_vec().into(),
+		address: address.0,
+		testnet_address: testnet_address.0,
+		shard,
+	};
+
+	base::output(&output)
+}
+
+fn secret_key(matches: &ArgMatches) -> Result<Vec<String>, String> {
+	let input = base::input_string(matches)?;
+
+	let input: Vec<u8> = input.parse::<Hex>().map_err(|_| "Convert failed")?.into();
+
+	let key_pair = KeyPair::from_secret_key(&input.clone())?;
+
+	let public_key = key_pair.public_key();
+
+	let address = public_key.to_address(Hrp::MAINNET).map_err(|_e| "address encode failed")?;
+	let testnet_address = public_key.to_address(Hrp::TESTNET).map_err(|_e| "address encode failed")?;
+
+	#[derive(Serialize)]
+	struct Shard {
+		shard_num: u16,
+		shard_count: u16,
+	}
+
+	let shard = SHARD_COUNT_LIST.iter().map(|&shard_count| {
+		let shard_num = utils::shard_num_for_bytes(&public_key, shard_count).expect("qed");
+		Shard {
+			shard_num,
+			shard_count,
+		}
+	}).collect::<Vec<_>>();
+
+	#[derive(Serialize)]
+	struct Output {
+		secret_key: Hex,
+		public_key: Hex,
+		address: String,
+		testnet_address: String,
+		shard: Vec<Shard>,
+	}
+
+	let output = Output {
+		secret_key: input.into(),
+		public_key: public_key.to_vec().into(),
+		address: address.0,
+		testnet_address: testnet_address.0,
+		shard,
+	};
+
+	base::output(&output)
+}
+
+fn public_key(matches: &ArgMatches) -> Result<Vec<String>, String> {
+	let input = base::input_string(matches)?;
+
+	let input: Vec<u8> = input.parse::<Hex>().map_err(|_| "Convert failed")?.into();
+
+	let public_key = input;
+
+	let address = public_key.to_address(Hrp::MAINNET).map_err(|_e| "address encode failed")?;
+	let testnet_address = public_key.to_address(Hrp::TESTNET).map_err(|_e| "address encode failed")?;
+
+	#[derive(Serialize)]
+	struct Shard {
+		shard_num: u16,
+		shard_count: u16,
+	}
+
+	let shard = SHARD_COUNT_LIST.iter().map(|&shard_count| {
+		let shard_num = utils::shard_num_for_bytes(&public_key, shard_count).expect("qed");
+		Shard {
+			shard_num,
+			shard_count,
+		}
+	}).collect::<Vec<_>>();
+
+	#[derive(Serialize)]
+	struct Output {
+		public_key: Hex,
+		address: String,
+		testnet_address: String,
+		shard: Vec<Shard>,
+	}
+
+	let output = Output {
+		public_key: public_key.into(),
+		address: address.0,
+		testnet_address: testnet_address.0,
+		shard,
+	};
+
+	base::output(&output)
+}
+
+fn address(matches: &ArgMatches) -> Result<Vec<String>, String> {
+	let input = base::input_string(matches)?;
+
+	let address = Address(input);
+
+	let (public_key, hrp) = <[u8; 32]>::from_address(&address).map_err(|_|"address decode failed")?;
+
+	#[derive(Serialize)]
+	struct Shard {
+		shard_num: u16,
+		shard_count: u16,
+	}
+
+	let shard = SHARD_COUNT_LIST.iter().map(|&shard_count| {
+		let shard_num = utils::shard_num_for_bytes(&public_key, shard_count).expect("qed");
+		Shard {
+			shard_num,
+			shard_count,
+		}
+	}).collect::<Vec<_>>();
+
+	#[derive(Serialize)]
+	struct Output {
+		address: String,
+		public_key: Hex,
+		hrp: String,
+		shard: Vec<Shard>,
+	}
+
+	let output = Output {
+		address: address.0.clone(),
+		public_key: public_key.to_vec().into(),
+		hrp: hrp.into(),
+		shard,
 	};
 
 	base::output(&output)
